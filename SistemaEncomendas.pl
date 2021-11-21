42b93e4f066654db84244a9ca45f1700e64edf5f
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2021-11-21 13:00:56 +0000

    Alterei o tempo e o espaço, e tirei acentos
    Não consigo fazer isto pelo terminal...

--- SistemaEncomendas.pl
+++ SistemaEncomendas.pl
@@ -1,11 +1,13 @@
 % Objetivo da Empresa Green Distribution -> Privilegiar sempre o meio de transporte + ecológico
 
-% Todos os id's usados devem ser únicos! De forma a que a unificação só ocorra uma vez no máximo para um id.
+% Todos os ids usados devem ser únicos! De forma a que a unificação só ocorra uma vez no maximo para um id.
 
 % 1. Definição das ruas de uma freguesia
 %rua(id, freguesia, nome, ...). % Como é que definimos?
 rua(1, vilaDoConde, rua1). 
+rua(2, povoa_Varzim, rua1). 
 
+distancia(vilaDoConde, povoa_Varzim, 10). 
 
 % 2. Definição do cliente
 %cliente(id, nome).
@@ -15,13 +17,21 @@ cliente(1, marco).
 % 3. Definição da encomenda
 %encomenda(clienteID, peso, volume, prazoEntrega, dataDeEncomenda, ruaEntrega). % Vai ser atribuido a um estafeta // dataDeEncomenda -> Data em que foi efetuada a encomenda pelo cliente
 %                                   dias/horas, hora/min/dia/mes/ano
-encomenda(1, 20, 25, 0/24, 16/43 / 08/08/2021, 1).
+encomenda(1, 20, 25, data(1,1,1), data(4,5,1), 2).
 
 
 % 4. Definição do estafeta
-%estafeta(id, nome, rating).
-estafeta(1, diogo, 4).
+%Acho que é preciso atribuir a cidade de origem
+%estafeta(id, nome, rating, cidade).
+estafeta(1, diogo, 4, povoa_Varzim).
 
+%Atribuir ratings
+estafetaEntregaSucesso(estafeta(Id, Nome, 5), estafeta(Id, Nome, 5)):- !.
+estafetaEntregaSucesso(estafeta(Id, Nome, Rating), estafeta(Id, Nome, RatingNovo)) :- RatingNovo is Rating+1.
+
+
+estafetaEntregaFalhou(estafeta(Id, Nome, 0), estafeta(Id, Nome, 0)):- !.
+estafetaEntregaFalhou(estafeta(Id, Nome, Rating), estafeta(Id, Nome, RatingNovo)) :- RatingNovo is Rating-1.
 % Prazo não cumprido por parte do estafeta -> Diminuir o seu número de entregas
 
 % 0 <= Rating <= 5
@@ -37,7 +47,18 @@ transporte(bicicleta, 5, 10).
 transporte(moto, 20, 35).
 transporte(carro, 100, 25).
 
-% Meio de Transporte        Peso máximo das encomendas (Kg)        Velocidade Máxima (Km/h)
+%Decide meio transporte
+
+decideTransporte(_, encomenda(_, Peso, _, _, _, _), transporte(carro, 100, 25)) :- Peso > 20.
+decideTransporte(estafeta(_, _, _, Cidade), encomenda(_, Peso, Data1, Data2, _, Rua), transporte(bicicleta, 5, 10)) :-
+    intervaloTempo(Data1, Data2, DataEntrega),
+    distancia(Rua, Cidade, Distancia),
+    velocidadeEntrega is DataEntrega/Distancia,
+    velocidadeEntrega < 10.
+    
+
+
+% Meio de Transporte        Peso maximo das encomendas (Kg)        Velocidade Máxima (Km/h)
 % Bicicleta                 5                                      10                           
 % Moto                      20                                     35
 % Carro                     100                                    25
@@ -47,12 +68,20 @@ transporte(carro, 100, 25).
 ecologicos([bicicleta, moto, carro]).
 
 
+%Datas
+data(Dia, Mes, Ano).
+%Calcula intervalo de tempo entre duas datas
+intervaloTempo(data(Dia1, Mes1, Ano1), data(Dia2, Mes2, Ano2), data(DiaEntrega, MesEntrega, AnoEntrega)) :- 
+    DiaEntrega is Dia2-Dia1,
+    MesEntrega is Mes2-Mes1,
+    AnoEntrega is Ano2-Ano1.
+
+
 % Falta resumir os objetivos/queries aqui
 %
 % Query 1
 % Identificar o estafeta que utilizou mais vezes um meio de transporte mais ecológico;
-estafetaMaisEcologico(Estafeta) :- findAll(estafeta, ,
-                                  include(estafeta).
+estafetaMaisEcologico(Estafeta) :- findAll(estafeta, include(estafeta)).
 %
 %
 %
