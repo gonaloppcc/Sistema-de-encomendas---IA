a6ddd77a8a6f87732c323d8c37a9348fc9b9d6e7
Mapc1 <marcocosta579@gmail.com>
2021-12-01 19:11:33 +0000

    Add invariantes de remoção

--- baseConhecimento.pl
+++ baseConhecimento.pl
@@ -25,6 +25,11 @@
     HoraEnc,
     rua(Rua,_,_) 
 ).
+-encomenda(EncID,_,_,_,_,_,_,_,_) :: 
+(
+    \+ entrega(_,_,EncID,_,_,_),
+    \+ atribuido(_,EncID)
+).
 
 +entrega(IDEstafeta,Veiculo,EncID,Rating,Data,Hora) :: 
 (
@@ -40,12 +45,18 @@
     Data,
     Hora
 ).
+-entrega(_,_,_,_,_,_) :: (true).
 
 +estafeta(ID,_,_) ::
 (
     findall(ID,estafeta(ID,_,_),R),
     length(R,L),
     L == 1
+).
+-estafeta(ID,_,_) ::
+(
+    \+ entrega(ID,_,_,_,_,_),
+    \+ atribuido(ID,_)
 ).
 
 +transporte(Veiculo,Carga,Velocidade,Preco) ::
@@ -57,6 +68,10 @@
     Velocidade > 0,
     Preco > 0
 ).
+-transporte(Veiculo,_,_,_) ::
+(
+    \+ entrega(_,Veiculo,_,_,_,_)
+).
 
 +cliente(ID,_) ::
 (
@@ -64,12 +79,20 @@
     length(R,L),
     L == 1
 ). 
+-cliente(ID,_) ::
+(
+    \+ encomenda(_,ID,_,_,_,_,_,_,_)
+).
 
 +rua(ID,_,_) ::
 (
     findall(ID, rua(ID,_,_),R),
     length(R,L),
     L == 1
+).
+-rua(ID,_,_) ::
+(
+    \+ encomenda(_,_,_,_,_,_,_,_,ID)
 ).
 
 +atribuido(Estafeta, EncID) ::
@@ -80,15 +103,27 @@
     estafeta(Estafeta,_,_),
     encomenda(EncID,_,_,_,_,_,_,_,_)
 ).
+-atribuido(Estafeta,EncID) ::
+(
+    \+ entrega(_,_,EncID,_,_,_)
+).
 
 evolucao( Termo ) :- 
     findall(Invariante,+Termo::Invariante,Lista),
     insercao(Termo),
     teste(Lista)
+. 
+
+remove(Termo) :-
+    findall(Invariante,-Termo::Invariante,Lista),
+    teste(Lista),
+    remocao(Termo)
 .
 
 insercao(Termo) :- assert(Termo).
 insercao(Termo) :- retract(Termo),!,fail.
+
+remocao(Termo) :- retract(Termo).
 
 teste([]).
 teste([H|T]) :- H, teste(T).
@@ -99,6 +134,7 @@ teste([H|T]) :- H, teste(T).
 rua(1, vilaDoConde, rua1). 
 rua(2, povoaVarzim, rua1). 
 rua(3, trofa,       avenida). 
+rua(4, braga,       rua4).
 
 
 %distancia: rua1, rua2 -> {V,F}
@@ -107,7 +143,10 @@ distancia(vilaDoConde, povoa_Varzim, 10).
 
 %cliente: id, nome -> {V,F} 
 cliente(1, marco).
-
+cliente(2, diogo).
+cliente(3, rita).
+cliente(4, goncalo).
+cliente(5, alice).
 
 %encomenda: encomendaID, clienteID, peso, volume, prazoEntrega, horasPrazoEntrega, dataDeEncomenda, horasDataEncomenda, ruaID -> {V,F}. 
 %Vai ser atribuido a um estafeta // dataDeEncomenda -> Data em que foi efetuada a encomenda pelo cliente
@@ -126,7 +165,7 @@ encomenda(9, 2, 70, 5,  data(12,3,2020), hora(15,40),  data(5,2,2020), hora(9,10
 %Não foram entregues 1
 entrega(1, bicicleta,     1, 2, data(23,12,2019), hora(18,40)).
 entrega(1, bicicleta, 2, 5, data(23,12,2019), hora(19,00)).
-entrega(3, mota,      3, 4, data(23,12,2019), hora(19,20)).
+entrega(3, moto,      3, 4, data(23,12,2019), hora(19,20)).
 entrega(2, carro,     4, 4, data(23,12,2),    hora(18,40)).
 %entrega(1, moto,      4, 5, data(23,12,2),    hora(18,40)).
 entrega(4, bicicleta, 6, 5, data(23,12,2039), hora(18,40)).
@@ -134,11 +173,10 @@ entrega(2, bicicleta, 7, 1, data(23,12,2),    hora(18,40)).
 
 %atribuidos IdEstafeta, IdEncomenda
 atribuido(1, 1).
+atribuido(3, 2).
 atribuido(1, 3).
-%atribuido(3, 2).
-atribuido(2, 3).
 atribuido(1, 4).
-atribuido(4, 5).
+atribuido(4, 6).
 atribuido(2, 6).
 atribuido(1, 7).
 atribuido(1, 8).
@@ -156,4 +194,5 @@ estafeta(4, goncalo,    lisboa).
 transporte(bicicleta, 5,   10, 5).
 transporte(moto,      20,  35, 10).
 transporte(carro,     100, 25, 20).
+transporte(barco,20,21,41).
 
