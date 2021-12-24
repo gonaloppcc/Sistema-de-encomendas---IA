797fd2483b9da62384a0fffa6093e8ba572c9a5c
Mapc1 <marcocosta579@gmail.com>
2021-12-24 19:04:51 +0000

    Add simple code

--- Fase2/fase2/src/main/java/uni/ia/App.java
+++ Fase2/fase2/src/main/java/uni/ia/App.java
@@ -1,5 +1,9 @@
 package uni.ia;
 
+import org.jpl7.Atom;
+import org.jpl7.JPL;
+import org.jpl7.Variable;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Atom jose = new Atom("jose");
+
+        Variable v = new Variable("X");
+        JPL.init(args);
+
+        for(String s : JPL.getActualInitArgs())
+            System.out.println(s);
+
+
+        System.out.println(v.name);
+        System.out.println(jose.name());
+        
     }
 }
