8bb32b03a8a0b010d779e8b162b5ae47724efb95
Mapc1 <marcocosta579@gmail.com>
2022-01-01 23:40:40 +0000

    Change algorithm in main

--- Fase2/main/main.py
+++ Fase2/main/main.py
@@ -1,4 +1,5 @@
 from algoritmos_procura.dfs import dfs
+from algoritmos_procura.gulosa import resolve_gulosa
 from gera_encomendas.geraEntregas import gerar_entregas
 import logging
 
@@ -6,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 
 def main():
-    algoritmo = dfs
+    algoritmo = resolve_gulosa
 
     logging.basicConfig(filename='fase2.log', level=logging.INFO)
     logging.debug('Started')
