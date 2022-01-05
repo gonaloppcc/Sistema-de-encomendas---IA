bcb4befd37e84a6036ff2a797ca9edced93b3c85
ritateixeira89494 <a89494@alunos.uminho.pt>
2022-01-05 18:20:45 +0000

    Menu

--- Fase2/main/Menu.py
+++ Fase2/main/Menu.py
@@ -1,45 +1,52 @@
-menu_options = {
-    1: 'Option 1',
-    2: 'Option 2',
-    3: 'Option 3',
-    4: 'Exit',
-}
+import sys
+
+from consolemenu import *
+from consolemenu.items import *
 
 
-def print_menu():
-    for key in menu_options.keys():
-        print(key, '--', menu_options[key])
+def menu():
+    # Create the root menu
+    menu = ConsoleMenu("Root Menu", "This is the Root Menu Subtitle")
+
+    item1 = MenuItem("Item 1", menu)
+
+    # Create a menu item that calls a function
+    function_item = FunctionItem("Fun item", Screen().input, kwargs={"prompt": "Enter an input: "})
+
+    # Create a menu item that calls a system command, based on OS type
+    if sys.platform.startswith('win'):
+        command_item = CommandItem("Command", 'cmd /c \"echo this is a shell. Press enter to continue." && set /p=\"')
+    else:
+        command_item = CommandItem("Command", 'sh -c \'echo "this is a shell. Press enter to continue."; read\'')
+
+    # Create a submenu using a Selection Menu, which takes a list of strings to create the menu items.
+    submenu = SelectionMenu(["item1", "item2", "item3"], title="Selection Menu",
+                            subtitle="These menu items return to the previous menu")
+
+    # Create the menu item that opens the Selection submenu
+    submenu_item = SubmenuItem("Submenu item", submenu=submenu)
+    submenu_item.set_menu(menu)
+
+    # Create a second submenu, but this time use a standard ConsoleMenu instance
+    submenu_2 = ConsoleMenu("Another Submenu Title", "Submenu subtitle.")
+    function_item_2 = FunctionItem("Fun item", Screen().input, ["Enter an input: "])
+    item2 = MenuItem("Another Item")
+    submenu_2.append_item(function_item_2)
+    submenu_2.append_item(item2)
+    submenu_item_2 = SubmenuItem("Another submenu", submenu=submenu_2)
+    submenu_item_2.set_menu(menu)
+
+    # Add all the items to the root menu
+    menu.append_item(item1)
+    menu.append_item(function_item)
+    menu.append_item(command_item)
+    menu.append_item(submenu_item)
+    menu.append_item(submenu_item_2)
+
+    # Show the menu
+    menu.start()
+    menu.join()
 
 
-def option1():
-    print('Opção \'Menu 1\'')
-
-
-def option2():
-    print('Opção \'Menu 2\'')
-
-
-def option3():
-    print('Opção \'Menu 3\'')
-
-
-if __name__ == '__main__':
-    while (True):
-        print_menu()
-        option = ''
-        try:
-            option = int(input('Escolha uma opção: '))
-        except:
-            print('Input incorreto. Por favor introduza um número...')
-        # Check what choice was entered and act accordingly
-        if option == 1:
-            option1()
-        elif option == 2:
-            option2()
-        elif option == 3:
-            option3()
-        elif option == 4:
-            print('Adeus...')
-            exit()
-        else:
-            print('Opção invélida, tente novamete.')
+if __name__ == "__main__":
+    menu()
